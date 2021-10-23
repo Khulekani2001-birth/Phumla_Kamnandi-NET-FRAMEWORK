cd02161e0acf821a3d7e8d5a4feb0dca6451bc2e
beyonsay <73654501+beyonsay@users.noreply.github.com>
2021-10-23 17:41:12 +0200

    CreateGuest FOrm

---
+++ Presentation/CreateGuestForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Phumla_Kamnandi.Presentation
+{
+    partial class CreateGuestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.iDTextBox = new System.Windows.Forms.TextBox();
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.adressTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.surnameLabel = new System.Windows.Forms.Label();
+            this.iDLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(187, 88);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // iDTextBox
+            // 
+            this.iDTextBox.Location = new System.Drawing.Point(187, 201);
+            this.iDTextBox.Name = "iDTextBox";
+            this.iDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.iDTextBox.TabIndex = 2;
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Location = new System.Drawing.Point(187, 148);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.surnameTextBox.TabIndex = 3;
+            // 
+            // adressTextBox
+            // 
+            this.adressTextBox.Location = new System.Drawing.Point(187, 257);
+            this.adressTextBox.Name = "adressTextBox";
+            this.adressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.adressTextBox.TabIndex = 4;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(75, 91);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 5;
+            this.nameLabel.Text = "Name";
+            // 
+            // surnameLabel
+            // 
+            this.surnameLabel.AutoSize = true;
+            this.surnameLabel.Location = new System.Drawing.Point(75, 148);
+            this.surnameLabel.Name = "surnameLabel";
+            this.surnameLabel.Size = new System.Drawing.Size(49, 13);
+            this.surnameLabel.TabIndex = 6;
+            this.surnameLabel.Text = "Surname";
+            // 
+            // iDLabel
+            // 
+            this.iDLabel.AutoSize = true;
+            this.iDLabel.Location = new System.Drawing.Point(75, 204);
+            this.iDLabel.Name = "iDLabel";
+            this.iDLabel.Size = new System.Drawing.Size(49, 13);
+            this.iDLabel.TabIndex = 7;
+            this.iDLabel.Text = "Guest ID";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(75, 260);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(45, 13);
+            this.addressLabel.TabIndex = 8;
+            this.addressLabel.Text = "Address";
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(655, 260);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.TabIndex = 9;
+            this.enterButton.Text = "Add guest";
+            this.enterButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(655, 161);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // CreateGuestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.iDLabel);
+            this.Controls.Add(this.surnameLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.adressTextBox);
+            this.Controls.Add(this.surnameTextBox);
+            this.Controls.Add(this.iDTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "CreateGuestForm";
+            this.Text = "CreateGuestForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox iDTextBox;
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.TextBox adressTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label surnameLabel;
+        private System.Windows.Forms.Label iDLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
---
+++ Presentation/CreateGuestForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Phumla_Kamnandi.Business;
+
+namespace Phumla_Kamnandi.Presentation
+{
+    public partial class CreateGuestForm : Form
+    {
+        #region Variables
+        public bool createFormClosed = false;
+        private Guest guest;
+        private GuestController guestController;
+        #endregion
+
+        #region Constructor
+        public CreateGuestForm(GuestController guestControl)
+        {
+            InitializeComponent();
+            guestController = guestControl;
+        }
+        #endregion
+
+        #region Utility Methods
+        private void ShowAll(bool value)
+        {
+            adressTextBox.Visible = value;
+            nameTextBox.Visible = value;
+            surnameTextBox.Visible = value;
+            iDTextBox.Visible = value;
+
+            iDLabel.Visible = value;
+            nameLabel.Visible = value;
+            surnameLabel.Visible = value;
+            addressLabel.Visible = value;
+        }
+
+        private void ClearAll()
+        {
+            adressTextBox.Text = "";
+            nameTextBox.Text = "";
+            surnameTextBox.Text = "";
+            iDTextBox.Text = "";
+        }
+
+        private void PopulateObject()
+        {
+            guest = new Guest();
+            guest.ID = adressTextBox.Text;
+            guest.Name = nameTextBox.Text;
+            guest.Surname = surnameTextBox.Text;
+            guest.Address = iDTextBox.Text;
+        }
+        #endregion
+
+        #region Form events
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            PopulateObject();
+            MessageBox.Show("To be submitted to the Database!");
+            guestController.DataMaintenance(guest, Data.DB.DBOperation.Add);
+            guestController.FinalizeChanges(guest);
+            ClearAll();
+            ShowAll(false);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //FOrgot
+        }
+
+        private void CreateGuestForm_Load(object sender, EventArgs e)
+        {
+            ShowAll(false);
+        }
+
+        private void CreateGuestForm_Activated(object sender, EventArgs e)
+        {
+            ShowAll(false);
+        }
+        #endregion
+    }
+}
