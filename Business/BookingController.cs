168badb75992548e60b1d2c7d2314dfa20895187
beyonsay <73654501+beyonsay@users.noreply.github.com>
2021-10-23 17:32:59 +0200

    Filled in BookingController

--- Business/BookingController.cs
+++ Business/BookingController.cs
@@ -3,10 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Phumla_Kamnandi.Data;
+using System.Collections.ObjectModel;
 
 namespace Phumla_Kamnandi.Business
 {
     class BookingController
     {
+        #region Data Members
+        BookingDB bookingDB;
+        Collection<Booking> bookings;
+        #endregion
+
+        #region Properties
+        public Collection<Booking> allBookings
+        {
+            get
+            {
+                return bookings;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public BookingController()
+        {
+            bookingDB = new BookingDB();
+            bookings = bookingDB.AllBookings;
+        }
+        #endregion
+
+        #region Database Communication.
+        public void DataMaintenance(Booking aBooking, DB.DBOperation operation)
+        {
+            int index = 0;
+
+            bookingDB.DataSetChange(aBooking, operation);
+
+            switch (operation)
+            {
+                case DB.DBOperation.Add:
+                    bookings.Add(aBooking); //*** Add the employee to the Collection
+                    break;
+                case DB.DBOperation.Edit:
+                    index = FindIndex(aBooking);
+                    bookings[index] = aBooking;
+                    break;
+                case DB.DBOperation.Delete:
+                    index = FindIndex(aBooking);
+                    bookings.Remove(bookings[index]);
+                    break;
+            }
+
+
+        }
+
+        //***Commit the changes to the database
+        public bool FinalizeChanges(Booking booking)
+        {
+            //***call the EmployeeDB method that will commit the changes to the database
+            return bookingDB.UpdateDataSource(booking);
+        }
+        #endregion
+
+        #region Searching through a collection
+        /*public Collection<Booking> FindByRole(Collection<Booking> booking)  MAYBE CHANGE TO FIND BY DATE??
+        {
+            Collection<Booking> matches = new Collection<Booking>();
+            foreach (Booking booking in bookings)
+            {
+                if (emp.role.getRoleValue == roleVal)
+                {
+                    matches.Add(booking);
+                }
+            }
+            return matches;
+        }*/
+
+        public Booking Find(string ID)
+        {
+            int index = 0;
+            bool found = (bookings[index].ID == ID);  
+            int count = bookings.Count;
+            while (!(found) && (index < bookings.Count - 1))  
+            {
+                index = index + 1;
+                found = (bookings[index].ID == ID);
+            }
+            return bookings[index];   
+        }
+
+        public int FindIndex(Booking aBooking)
+        {
+            int counter = 0;
+            bool found = false;
+            found = (aBooking.ID == bookings[counter].ID);
+            while (!found && counter < bookings.Count)
+            {
+                counter++;
+                found = (aBooking.ID == bookings[counter].ID);
+            }
+
+            if (found)
+            {
+                return counter;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        #endregion
     }
 }
