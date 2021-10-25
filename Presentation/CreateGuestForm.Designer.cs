4e6668c1433caad7dff3c9946eb1b2d68e3bd14c
beyonsay <73654501+beyonsay@users.noreply.github.com>
2021-10-26 00:18:47 +0200

    UI attempt cteateguestform

--- Presentation/CreateGuestForm.Designer.cs
+++ Presentation/CreateGuestForm.Designer.cs
@@ -41,32 +41,33 @@ namespace Phumla_Kamnandi.Presentation
             this.exitButton = new System.Windows.Forms.Button();
             this.PhoneLabel = new System.Windows.Forms.Label();
             this.PhoneTextBox = new System.Windows.Forms.TextBox();
+            this.promptLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(187, 87);
+            this.nameTextBox.Location = new System.Drawing.Point(297, 123);
             this.nameTextBox.Name = "nameTextBox";
             this.nameTextBox.Size = new System.Drawing.Size(100, 20);
             this.nameTextBox.TabIndex = 0;
             // 
             // iDTextBox
             // 
-            this.iDTextBox.Location = new System.Drawing.Point(187, 201);
+            this.iDTextBox.Location = new System.Drawing.Point(297, 231);
             this.iDTextBox.Name = "iDTextBox";
             this.iDTextBox.Size = new System.Drawing.Size(100, 20);
             this.iDTextBox.TabIndex = 2;
             // 
             // surnameTextBox
             // 
-            this.surnameTextBox.Location = new System.Drawing.Point(187, 148);
+            this.surnameTextBox.Location = new System.Drawing.Point(297, 177);
             this.surnameTextBox.Name = "surnameTextBox";
             this.surnameTextBox.Size = new System.Drawing.Size(100, 20);
             this.surnameTextBox.TabIndex = 3;
             // 
             // adressTextBox
             // 
-            this.adressTextBox.Location = new System.Drawing.Point(187, 257);
+            this.adressTextBox.Location = new System.Drawing.Point(297, 282);
             this.adressTextBox.Name = "adressTextBox";
             this.adressTextBox.Size = new System.Drawing.Size(100, 20);
             this.adressTextBox.TabIndex = 4;
@@ -74,44 +75,49 @@ namespace Phumla_Kamnandi.Presentation
             // nameLabel
             // 
             this.nameLabel.AutoSize = true;
-            this.nameLabel.Location = new System.Drawing.Point(75, 90);
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(152, 130);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.Size = new System.Drawing.Size(51, 20);
             this.nameLabel.TabIndex = 5;
             this.nameLabel.Text = "Name";
             // 
             // surnameLabel
             // 
             this.surnameLabel.AutoSize = true;
-            this.surnameLabel.Location = new System.Drawing.Point(71, 148);
+            this.surnameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.surnameLabel.Location = new System.Drawing.Point(152, 184);
             this.surnameLabel.Name = "surnameLabel";
-            this.surnameLabel.Size = new System.Drawing.Size(49, 13);
+            this.surnameLabel.Size = new System.Drawing.Size(74, 20);
             this.surnameLabel.TabIndex = 6;
             this.surnameLabel.Text = "Surname";
             // 
             // iDLabel
             // 
             this.iDLabel.AutoSize = true;
-            this.iDLabel.Location = new System.Drawing.Point(75, 204);
+            this.iDLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iDLabel.Location = new System.Drawing.Point(151, 231);
             this.iDLabel.Name = "iDLabel";
-            this.iDLabel.Size = new System.Drawing.Size(49, 13);
+            this.iDLabel.Size = new System.Drawing.Size(74, 20);
             this.iDLabel.TabIndex = 7;
             this.iDLabel.Text = "Guest ID";
             // 
             // addressLabel
             // 
             this.addressLabel.AutoSize = true;
-            this.addressLabel.Location = new System.Drawing.Point(75, 260);
+            this.addressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressLabel.Location = new System.Drawing.Point(152, 289);
             this.addressLabel.Name = "addressLabel";
-            this.addressLabel.Size = new System.Drawing.Size(45, 13);
+            this.addressLabel.Size = new System.Drawing.Size(68, 20);
             this.addressLabel.TabIndex = 8;
             this.addressLabel.Text = "Address";
             // 
             // enterButton
             // 
-            this.enterButton.Location = new System.Drawing.Point(655, 260);
+            this.enterButton.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterButton.Location = new System.Drawing.Point(612, 289);
             this.enterButton.Name = "enterButton";
-            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.Size = new System.Drawing.Size(89, 71);
             this.enterButton.TabIndex = 9;
             this.enterButton.Text = "Add guest";
             this.enterButton.UseVisualStyleBackColor = true;
@@ -119,9 +125,10 @@ namespace Phumla_Kamnandi.Presentation
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(655, 161);
+            this.exitButton.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(612, 381);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.Size = new System.Drawing.Size(89, 57);
             this.exitButton.TabIndex = 10;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
@@ -130,18 +137,29 @@ namespace Phumla_Kamnandi.Presentation
             // PhoneLabel
             // 
             this.PhoneLabel.AutoSize = true;
-            this.PhoneLabel.Location = new System.Drawing.Point(75, 308);
+            this.PhoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PhoneLabel.Location = new System.Drawing.Point(152, 340);
             this.PhoneLabel.Name = "PhoneLabel";
-            this.PhoneLabel.Size = new System.Drawing.Size(38, 13);
+            this.PhoneLabel.Size = new System.Drawing.Size(55, 20);
             this.PhoneLabel.TabIndex = 12;
             this.PhoneLabel.Text = "Phone";
             // 
             // PhoneTextBox
             // 
-            this.PhoneTextBox.Location = new System.Drawing.Point(187, 305);
+            this.PhoneTextBox.Location = new System.Drawing.Point(297, 340);
             this.PhoneTextBox.Name = "PhoneTextBox";
             this.PhoneTextBox.Size = new System.Drawing.Size(100, 20);
             this.PhoneTextBox.TabIndex = 11;
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promptLabel.Location = new System.Drawing.Point(176, 9);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(591, 64);
+            this.promptLabel.TabIndex = 13;
+            this.promptLabel.Text = "Enter new guest details";
             // 
             // CreateGuestForm
             // 
@@ -149,6 +167,7 @@ namespace Phumla_Kamnandi.Presentation
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.promptLabel);
             this.Controls.Add(this.PhoneLabel);
             this.Controls.Add(this.PhoneTextBox);
             this.Controls.Add(this.exitButton);
@@ -182,5 +201,6 @@ namespace Phumla_Kamnandi.Presentation
         private System.Windows.Forms.Button exitButton;
         private System.Windows.Forms.Label PhoneLabel;
         private System.Windows.Forms.TextBox PhoneTextBox;
+        private System.Windows.Forms.Label promptLabel;
     }
 }
